e1b5a67eb582c4efca984cc36c0ea9e82f7dec92
samirakolhe <94692487+samirakolhe@users.noreply.github.com>
2025-06-04 23:41:41 -0700

    Hello -> Hell

--- hello.java
+++ hello.java
@@ -10,7 +10,7 @@ public class HelloWorld extends HttpServlet {
 
    public void init() throws ServletException {
       // Do required initialization
-      message = "Hello World";
+      message = "Hell World";
    }
 
    public void doGet(HttpServletRequest request, HttpServletResponse response)
